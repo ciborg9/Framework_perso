3f44fc3ee621fc2a3c7ef3d8f17e5fa1dae30203
meyerv_p <paul.meyervithu@epitech.eu>
2015-05-12 16:45:01 +0200

    peaufinage

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -5,10 +5,14 @@ abstract Class Controller {
 
     public function render($view, $param = array()) {
         $views = explode(':', $view);
-        var_dump($views);
-        if (empty($views[0])) { /*si le view = ":fichier"*/
-            $file = fopen('../app/views/' . $views[1] . '.html', 'r');
-            $file = fread($file, filesize('../app/views/' . $views[1] . '.html'));
+        if (empty($views[0])) {
+            $dir = '../app/views/' . $views[1] . '.html';
+        } else {
+            $dir = '../app/views/' . $views[0] . '/' . $views[1] . '.html';
+        }
+        //var_dump($views);
+        if (file_exists($dir)) {
+            $file = file_get_contents($dir);
             if (!empty($param)) {
                 //traitement de remplacement des clé par les valeur avec preg_replace
                 foreach ($param as $key => $value) {
@@ -16,16 +20,8 @@ abstract Class Controller {
                 }
             }
             echo $file;
-        } else { /*si le view = "dosier:fichier"*/
-            $file = fopen('../app/views/' . $views[0] . '/' . $views[1] . '.html', 'r');
-            $file = fread($file, filesize('../app/views/' . $views[0] . '/' . $views[1] . '.html'));
-            if (!empty($param)) {
-                //traitement de remplacement des clé par les valeur avec preg_replace
-                foreach ($param as $key => $value) {
-                    $file = preg_replace('/{{ (' . $key . ') }}/', $value, $file);
-                }
-            }
-            echo $file;
+        } else {
+            echo "fichier de vue incorrect !!!";
         }
     }
 }
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,8 +5,7 @@ use \app\Controllers\Controller;
 class IndexController extends Controller {
 
     public function indexAction() {
-        $this->render(":ViewFilename",array('data' => 'LICORNE'));
-        echo __file__."test";
+        return $this->render(":ViewFilename",array('data' => 'LICORNE'));
     }
 }
 ?>
